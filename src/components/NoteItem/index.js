3dc3cf0d30ea1774d3bf3e1337fc6c961b20fc99
AshwinKanth <ashwinvjrj@gmail.com>
2023-03-15 09:38:08 +0000

    notes app solution

---
+++ src/components/NoteItem/index.js
@@ -0,0 +1,15 @@
+import {ListItemContainer, TitleHeading, Description} from './styledComponents'
+
+const NoteItem = props => {
+  const {NoteItemDetails} = props
+  const {title, noteText} = NoteItemDetails
+
+  return (
+    <ListItemContainer>
+      <TitleHeading>{title}</TitleHeading>
+      <Description>{noteText}</Description>
+    </ListItemContainer>
+  )
+}
+
+export default NoteItem
